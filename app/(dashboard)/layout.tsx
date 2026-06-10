90ac360965e41bb9cda584177a7d3d106ba4347c
Elsayed <elsayedhossny90@gmail.com>
2026-06-10 09:50:33 +0300

    modify navbar site and feature navbar dashboard and make light-dark Mode and modifu UI login/register of site

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,30 @@
 import "../globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
-export default function DashboardLayout({
+import { ThemeProvider } from "../../components/theme-provider";
+import NavbarDash from "./NavbarDash";
+
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main className="bg-zinc-100 flex-1 flex flex-col min-h-screen">
-        <div className="p-4">
-          <SidebarTrigger />
-        </div>
-        <div className="flex-1 p-6">{children}</div>
-      </main>
-    </SidebarProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      // storageKey="theme"
+      disableTransitionOnChange
+    >
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="flex-1 w-full">
+          <NavbarDash />
+          {children}
+        </main>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
